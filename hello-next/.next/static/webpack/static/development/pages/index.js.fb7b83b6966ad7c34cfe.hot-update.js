95cdd18aa67bc8e4e9ca86f72f2e356228406433
connor <cwebdale006@west-mec.org>
2019-11-15 15:29:27 -0700

    init

---
+++ hello-next/.next/static/webpack/static/development/pages/index.js.fb7b83b6966ad7c34cfe.hot-update.js
@@ -0,0 +1,16 @@
+webpackHotUpdate("static/development/pages/index.js",[
+/* 0 */
+false,
+/* 1 */
+/*!******************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fcwebdale006%2FDocuments%2Funit%203%2Freact%20i%20guess%2Fhello-next%2Fpages%2Findex.js ***!
+  \******************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fcwebdale006%2FDocuments%2Funit%203%2Freact%20i%20guess%2Fhello-next%2Fpages%2Findex.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fcwebdale006%2FDocuments%2Funit%203%2Freact%20i%20guess%2Fhello-next%2Fpages%2Findex.js!./");
+
+
+/***/ })
+])
+//# sourceMappingURL=index.js.fb7b83b6966ad7c34cfe.hot-update.js.map
